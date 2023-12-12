0e778d14d21898c7d83ec070ad7dde0529b231b2
SaharaSurfer <st119029@student.spbu.ru>
2023-12-12 10:55:50 +0300

    Added parameters into FiniteAutomat class

--- FiniteAutomat/header/FiniteAutomat.h
+++ FiniteAutomat/header/FiniteAutomat.h
@@ -9,9 +9,17 @@ class FiniteAutomat
 public:
 	typedef typename Container::value_type Symbol;
 
-	FiniteAutomat(std::function<size_t(size_t, Symbol)> _transition_func, size_t _initial_state,
-		std::unordered_set<size_t> _final_states)
+	FiniteAutomat
+	(
+		std::unordered_set<size_t> _states,
+		std::unordered_set<Symbol> _alphabet,
+		std::function<size_t(size_t, Symbol)> _transition_func, 
+		size_t _initial_state,
+		std::unordered_set<size_t> _final_states
+	)
 	{
+		states = _states;
+		alphabet = _alphabet;
 		transition_func = _transition_func;
 		current_state = _initial_state;
 		final_states = _final_states;
@@ -21,15 +29,28 @@ public:
 	{
 		for (Symbol symbol : input_chain)
 		{
+			if (alphabet.find(symbol) == alphabet.end())
+			{
+				return false;
+			}
+
 			current_state = transition_func(current_state, symbol);
+
+			if (states.find(current_state) == states.end())
+			{
+				return false;
+			}
+
 			if (final_states.find(current_state) != final_states.end())
 			{
-				return 1;
+				return true;
 			}
 		}
-		return 0;
+		return false;
 	}
 private:
+	std::unordered_set<size_t> states;
+	std::unordered_set<Symbol> alphabet;
 	std::function<size_t(size_t, Symbol)> transition_func;
 	size_t current_state;
 	std::unordered_set<size_t> final_states;
--- FiniteAutomat/src/main.cpp
+++ FiniteAutomat/src/main.cpp
@@ -8,12 +8,14 @@
 int main()
 {
 	std::string pattern = "abab";
-	std::unordered_set<size_t> final_states = { pattern.size() };
-	
 	std::string text = "abababxyz";
+	std::unordered_set<size_t> states = { 0, 1, 2, 3, 4 };
+	std::unordered_set<char> alphabet(text.begin(), text.end());
+	std::unordered_set<size_t> final_states = { 4 };
+	
 
 	SubstringFinder kmp(pattern);
-	FiniteAutomat< std::string > automat(kmp, 0, final_states);
+	FiniteAutomat< std::string > automat(states, alphabet, kmp, 0, final_states);
 	std::cout << automat(text);
 	
 	return 0;
